bd2ab5da898726b67a15eef3cb8b8730c0f9f1e1
RogerJackD <rogerzapata114@gmail.com>
2025-10-08 23:11:29 -0500

    feat(components): add task Card base UI

---
+++ src/components/task-card.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Card, CardContent, CardHeader } from './ui/card'
+import { Task } from '@/types/kanbanResponse'
+
+interface TaskCardProps {
+  task: Task
+}
+
+export default function TaskCard({task} : TaskCardProps) {
+  return (
+    <Card>
+
+        <div className="cursor-pointer">
+            <CardHeader>
+                <div className="flex items-start justify-between gap-2 pr-6">
+                    <h3 className="font-medium text-sm leading-tight text-card-foreground">{task.title}</h3>
+                </div>
+            </CardHeader>
+            <CardContent className="pt-0 space-y-3">
+                <p className="text-xs text-muted-foreground line-clamp-2">descripcion de tarea</p>
+            </CardContent>
+        </div>
+    </Card>
+  )
+}
