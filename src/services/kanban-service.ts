8560b7112d5a52bc41deef2bd59b2212ea479c53
RogerJackD <rogerzapata114@gmail.com>
2025-10-08 23:07:39 -0500

    feat(services): add method getKanbanBoardById fetching

---
+++ src/services/kanban-service.ts
@@ -0,0 +1,31 @@
+import { KanbanBoardResponse } from '../types/kanbanResponse';
+
+
+const API_BASE_URL = 'http://localhost:3030/api';
+
+
+export const kanbanService = {
+
+    async getKanbanBoardById(idBoard: string): Promise<KanbanBoardResponse> {
+            const response = await fetch(`${API_BASE_URL}/boards/${idBoard}/tasks`,{
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            })
+
+            if( !response.ok){
+                let errorMessage = `Error ${response.status}: ${response.statusText}`
+                try {
+                    const errorData = await response.json();
+                    //usando mensaje especifico de mi servidor
+                    errorMessage = errorData.message || errorData;
+                } catch {
+                    console.log("error unexpected, check servers logs");
+                }
+
+                throw new Error(errorMessage)
+            }
+            return await response.json()
+        }
+}
