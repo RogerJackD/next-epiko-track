4f208c848385f7cbe1572cd4c3de28393d8f1b70
RogerJackD <rogerzapata114@gmail.com>
2025-11-07 11:23:31 -0500

    feat(services): implement Board service with CRUD operations (getAll, create, update)

--- src/services/board-service.ts
+++ src/services/board-service.ts
@@ -1,4 +1,4 @@
-import { Board, Project } from "@/types/board";
+import { Board, CreateBoardDTO, Project, UpdateBoardDTO } from "@/types/board";
 
 
 const API_BASE_URL = 'http://localhost:3030/api';
@@ -27,7 +27,7 @@ export const boardService = {
                 throw new Error(errorMessage)
             }
             return await response.json()
-        },
+    },
     async getAllBoards(): Promise<Project[]> {
             const response = await fetch(`${API_BASE_URL}/boards`,{
                 method: 'GET',
@@ -49,5 +49,49 @@ export const boardService = {
                 throw new Error(errorMessage)
             }
             return await response.json()
-        },
-    }    
+    },
+    async createBoard(createBoardDTO: CreateBoardDTO): Promise<Board> {
+            const response = await fetch(`${API_BASE_URL}/boards`,{
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(createBoardDTO),
+            })
+            if( !response.ok){
+                let errorMessage = `Error ${response.status}: ${response.statusText}`
+                try {
+                    const errorData = await response.json();
+                    //usando mensaje especifico de mi servidor
+                    errorMessage = errorData.message || errorData;
+                } catch {
+                    console.log("error unexpected, check servers logs");
+                }
+
+                throw new Error(errorMessage)
+            }
+            return await response.json()
+    },
+    async updateBoard(updateBoardDTO : UpdateBoardDTO, idBoard: string): Promise<Board> {
+            const response = await fetch(`${API_BASE_URL}/boards/${idBoard}`,{
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(updateBoardDTO),
+            })
+            if( !response.ok){
+                let errorMessage = `Error ${response.status}: ${response.statusText}`
+                try {
+                    const errorData = await response.json();
+                    //usando mensaje especifico de mi servidor
+                    errorMessage = errorData.message || errorData;
+                } catch {
+                    console.log("error unexpected, check servers logs");
+                }
+
+                throw new Error(errorMessage)
+            }
+            return await response.json()                
+    },
+}
