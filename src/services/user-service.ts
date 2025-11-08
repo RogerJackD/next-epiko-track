d9aa82cf4c5968dc208e741e8b67d259126193b3
RogerJackD <rogerzapata114@gmail.com>
2025-11-08 00:17:07 -0500

    feat(users): add user service with create and update functionality

--- src/services/user-service.ts
+++ src/services/user-service.ts
@@ -1,4 +1,4 @@
-import { User } from "@/types/user";
+import { CreateUserDTO, UpdateUserDTO, User } from "@/types/user";
 
 const API_BASE_URL = 'http://localhost:3030/api';
 
@@ -26,5 +26,47 @@ export const userService = {
                 throw new Error(errorMessage)
             }
             return await response.json()
+    },
+
+    async createUser(createUserData: CreateUserDTO) {
+        const response = await fetch(`${API_BASE_URL}/auth/users`,{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(createUserData),
+        })
+        if( !response.ok){
+            let errorMessage = `Error ${response.status}: ${response.statusText}`
+            try {
+                const errorData = await response.json();
+                errorMessage = errorData.message || errorData;
+            } catch {
+                console.log("error unexpected, check servers logs");
+            }
+            throw new Error(errorMessage)
         }
+        return await response.json()
+    },
+    async updateUser(updateUserData: UpdateUserDTO, userId : string) {
+        const response = await fetch(`${API_BASE_URL}/auth/users/${userId}`,{
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(updateUserData),
+        })
+        if( !response.ok){
+            let errorMessage = `Error ${response.status}: ${response.statusText}`
+            try {
+                const errorData = await response.json();
+                errorMessage = errorData.message || errorData;
+                console.log(errorMessage)
+            } catch {
+                console.log("error unexpected, check servers logs");
+            }
+            throw new Error(errorMessage)
+        }
+        return await response.json()
+    }
 }
