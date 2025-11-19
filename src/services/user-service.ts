ef6e79649b8774e788230a535b04b13dd8f0d8da
RogerJackD <rogerzapata114@gmail.com>
2025-11-19 00:03:42 -0500

    feat(services): add new method datafetch users with active status

--- src/services/user-service.ts
+++ src/services/user-service.ts
@@ -2,7 +2,33 @@ import { CreateUserDTO, UpdateUserDTO, User } from "@/types/user";
 
 export const userService = {
 
+
     async getUsers(): Promise<User[]> {
+            const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/auth/users`,{
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            })
+
+            if( !response.ok){
+                let errorMessage = `Error ${response.status}: ${response.statusText}`
+                try {
+                    const errorData = await response.json();
+                    //usando mensaje especifico de mi servidor
+                    errorMessage = errorData.message || errorData;
+                } catch {
+                    console.log("error unexpected, check servers logs");
+                }
+
+                throw new Error(errorMessage)
+            }
+            //solo retornar usuarios status = true
+            return (await response.json()).filter((user: User) => user.status === true);
+    },
+
+        //retorna todos los usuarios sin filtrar por status
+        async getAllUsers(): Promise<User[]> {
             const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/auth/users`,{
                 method: 'GET',
                 headers: {
@@ -24,6 +50,9 @@ export const userService = {
             }
             return await response.json()
     },
+
+
+    
 
     async createUser(createUserData: CreateUserDTO) {
         const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/auth/users`,{
