8e39863e2ec046e81f82ff0450c55b4c83a607ec
RogerJackD <rogerzapata114@gmail.com>
2025-11-27 02:20:41 -0500

    feat(notification-service): add getOverDueTasks method to fetch overdue tasks

--- src/services/notifications-service.ts
+++ src/services/notifications-service.ts
@@ -24,5 +24,28 @@ export const notificationService = {
                 throw new Error(errorMessage)
             }
             return await response.json()
-        }
+    },
+
+    async getOverDueTasks(): Promise<NotificationTaskResponse[]> {
+            const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/boards/tasks/overdue/due-date`,{
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            })
+
+            if( !response.ok){
+                let errorMessage = `Error ${response.status}: ${response.statusText}`
+                try {
+                    const errorData = await response.json();
+                    //usando mensaje especifico de mi servidor
+                    errorMessage = errorData.message || errorData;
+                } catch {
+                    console.log("error unexpected, check servers logs");
+                }
+
+                throw new Error(errorMessage)
+            }
+            return await response.json()
+    }
 }
